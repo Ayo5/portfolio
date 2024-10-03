c006659983d29821d4e92e0ffc3f69ec3cc7317a
Ayoub <abdelkader_dai@ens.iut-lens.fr>
2024-10-03 22:11:26 +0200

    Changement de la couleur des pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,21 @@ const About = () => {
     return (
         <div>
             {/* Affichage du Header */}
-            <Header />
+            <Header/>
             <NavBar/>
+            <div className="text-center">
+                <h2 className="text-4xl font-bold mb-4">À propos de moi</h2>
+
+                <p className="text-lg text-white">
+                    Je suis un développeur junior passionné par la création de solutions innovantes et
+                    l'amélioration de l'expérience utilisateur. Mon expertise inclut des technologies modernes
+                    et un engagement envers le développement durable.
+                </p>
+                <p className="mt-4 text-lg text-white">
+                    J'aime apprendre de nouvelles technologies et collaborer avec d'autres pour créer
+                    des projets significatifs.
+                </p>
+            </div>
         </div>
     );
 };
