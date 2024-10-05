ec814c2e50a5959a236593fc4018d5b6938e8e48
Ayoub <abdelkader_dai@ens.iut-lens.fr>
2024-10-05 20:11:41 +0200

    Complétion d'une Carte

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -11,18 +11,18 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, description, imageUrl, altText }) => {
     return (
         <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-white/10 hover:shadow-xl transition-shadow duration-300">
-            <JavaIcon size={100}/>
+            <JavaIcon size={75}/>
 
             <div className="px-6 py-4">
                 <h2 className="font-bold text-xl mb-2">{title}</h2>
-                <p className="text-gray-700 text-base">{description}</p>
+                <p className="text-white text-base">{description}</p>
             </div>
             <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #Portfolio
+        <span className="inline-block bg-white/50 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
+          #Java
         </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #WebDev
+                <span className="inline-block bg-white/50 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
+          #Scartch
         </span>
             </div>
         </div>
