4bebbb897a4a86bdb54ac01e21aca577c6d8ac92
Ayoub <abdelkader_dai@ens.iut-lens.fr>
2024-10-06 03:55:53 +0200

    Ajout de carte sur la page projet

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -1,29 +1,39 @@
 import Image from 'next/image';
-import {JavaIcon} from "@/app/_components/icons/JavaIcon";
+
 
 interface CardProps {
     title: string;
     description: string;
     imageUrl: string;
     altText: string;
+    href: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, imageUrl, altText }) => {
+const Card: React.FC<CardProps> = ({ title, description, imageUrl, altText, href }) => {
     return (
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-white/10 hover:shadow-xl transition-shadow duration-300">
-            <JavaIcon size={75}/>
-
+        <div className="max-w-lg rounded-3xl border-2 border-white/10 overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <Image
+                className="w-full"
+                src={imageUrl}
+                alt={altText}
+                width={600}
+                height={400}
+                objectFit="cover"
+                quality={100} // Amélioration de la qualité de l'image
+            />
             <div className="px-6 py-4">
-                <h2 className="font-bold text-xl mb-2">{title}</h2>
+                <a href={href} target="_blank" rel="noopener noreferrer"> {/* Lien appliqué uniquement au titre */}
+                    <h2 className="font-bold text-xl mb-2 hover:underline">{title}</h2>
+                </a>
                 <p className="text-white text-base">{description}</p>
             </div>
             <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-white/50 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
-          #Java
-        </span>
                 <span className="inline-block bg-white/50 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
-          #Scartch
-        </span>
+                    #Java
+                </span>
+                <span className="inline-block bg-white/50 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
+                    #Scratch
+                </span>
             </div>
         </div>
     );
--- app/projet/page.tsx
+++ app/projet/page.tsx
@@ -9,12 +9,22 @@ const Projet = () => {
             {/* Affichage du Header */}
             <Header />
             <NavBar/>
+            <div className="flex flex-row items-center justify-center p-5 gap-x-10 animate-slide-up-fade animation-delay-100">
+            <Card
+                title="Ray Tracer"
+                description="Une application en Java uniquement qui permet de simuler le comportement de la lumière sur un objet"
+                imageUrl="/Sphere.PNG"
+                altText="Capture d'écran du résultat"
+                href="https://github.com/Ayo5/RayTracerFromScratch"
+            />
             <Card
                 title="Projet 1"
                 description="Une application web de gestion de tâches."
-                imageUrl="/images/project1.png"
+                imageUrl="/Sphere.PNG"
                 altText="Capture d'écran du projet 1"
+                href="https://github.com/Ayo5/RayTracerFromScratch"
             />
+            </div>
 
         </div>
     );
