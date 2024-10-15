04521f7a309416c636ad67d0af238ac8248470a1
Ayoub Daï <daiayoub59@gmail.com>
2024-10-15 10:25:23 +0200

    Ajustement de la page projet

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -7,16 +7,17 @@ interface CardProps {
     imageUrl: string;
     altText: string;
     href: string;
+    tag : string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, imageUrl, altText, href }) => {
+const Card: React.FC<CardProps> = ({ title, description, imageUrl, altText, href , tag }) => {
     return (
-        <div className="max-w-lg rounded-3xl border-2 border-white/10 overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <div className="max-w-sm rounded-3xl border-2 border-white/10 overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
             <Image
-                className="w-full"
+                className="w-auto h-auto"
                 src={imageUrl}
                 alt={altText}
-                width={600}
+                width={300}
                 height={400}
                 objectFit="cover"
                 quality={100} // Amélioration de la qualité de l'image
@@ -29,10 +30,7 @@ const Card: React.FC<CardProps> = ({ title, description, imageUrl, altText, href
             </div>
             <div className="px-6 pt-4 pb-2">
                 <span className="inline-block bg-white/50 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
-                    #Java
-                </span>
-                <span className="inline-block bg-white/50 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
-                    #Scratch
+                    {tag}
                 </span>
             </div>
         </div>
