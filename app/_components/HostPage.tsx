ffdaea87c2063bc793dda34450fa2b1faa60e914
DonPablo <daiayoub59@gmail.com>
2025-06-08 00:37:38 +0200

    refactor: improve formatting and consistency in HostPage component

--- app/_components/HostPage.tsx
+++ app/_components/HostPage.tsx
@@ -3,29 +3,33 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export const HostPage = () => {
-    return (
-        <Section className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center">
-            {/* Buttons above the title */}
-            <div className="mb-5 space-x-1">
-                <Link href="/projet">
-                    <Button className="p-2 font-sans text-gray-400 hover:text-white transition duration-300">projet</Button>
-                </Link>
-                <Link href="/about">
-                    <Button className="p-2 font-sans text-gray-400 hover:text-white transition duration-300">à propos</Button>
-                </Link>
-            </div>
+  return (
+    <Section className='flex flex-col items-center justify-center min-h-screen bg-cover bg-center'>
+      {/* Buttons above the title */}
+      <div className='mb-5 space-x-1'>
+        <Link href='/projet'>
+          <Button className='p-2 font-sans text-gray-400 hover:text-white transition duration-300'>
+            projet
+          </Button>
+        </Link>
+        <Link href='/about'>
+          <Button className='p-2 font-sans text-gray-400 hover:text-white transition duration-300'>
+            à propos
+          </Button>
+        </Link>
+      </div>
 
-            {/* Title with typing animation */}
-            <h1 className="text-8xl font-bold text-center text-white border-r-2 border-white whitespace-nowrap overflow-hidden animate-typing animate-blink-caret">
-                Abdelkader Daï
-            </h1>
+      {/* Title with typing animation */}
+      <h1 className='sm:text-xl lg:text-8xl  font-bold text-center text-white border-r-2 border-white whitespace-nowrap overflow-hidden animate-typing animate-blink-caret'>
+        Abdelkader Daï
+      </h1>
 
-            {/* Text below the title */}
-            <p className="mt-5 text-lg text-center text-gray-300">
-                Développeur junior et créatif
-            </p>
+      {/* Text below the title */}
+      <p className='sm:text-xs lg:text-lg mt-5 text-center text-gray-300'>
+        Développeur junior et créatif
+      </p>
 
-            <style>{`
+      <style>{`
             @-webkit-keyframes typing { 
             from { width: 0; } }
             @-webkit-keyframes blink-caret { 50% { border-color: transparent; } }
@@ -41,6 +45,6 @@ export const HostPage = () => {
                        blink-caret .5s step-end infinite alternate;
             }
       `}</style>
-        </Section>
-    );
+    </Section>
+  );
 };
